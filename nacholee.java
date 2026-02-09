4b9c3fdf02576f49e1320138afc2082d34ac9582
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-02-09 09:11:37 -0500

    Se cambió el hola mundo y se agregó codigo para sumar dos numeros

--- nacholee.java
+++ nacholee.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class nacholee {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Vamos a sumar dos números ");
+        int n1=0, n2=0, suma=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Ingrese el numero 1: ");
+        n1=sc.nextInt();
+        System.out.println("Ingrese el numero 2: ");
+        n2=sc.nextInt();
+        System.out.println("La suma de los numeros es: "+(n1+n2));
+
     }
 }
